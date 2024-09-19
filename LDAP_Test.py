4d29d654a30270964160b779e8d9ae023d454ec8
sudhanvad18 <87053615+sudhanvad18@users.noreply.github.com>
2024-09-19 16:02:13 -0400

    Add files via upload

---
+++ LDAP_Test.py
@@ -0,0 +1,21 @@
+import ldap
+
+def authenticate(username, password):
+    ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
+    server = "ldaps://ldap.example.com:636"
+    base_dn = "dc=example.com"
+    user_dn = "uid={},{}".format(username, base_dn)
+    try:
+        l = ldap.initialize(server)
+        l.protocol_version = ldap.VERSION3
+        
+        l.simple_bind_s(user_dn, password)
+        return True
+    except ldap.INVALID_CREDENTIALS:
+        return False
+    except ldap.LDAPError as error:
+        print("Error:", error)
+        return False
+    finally:
+        # close the connection to the server
+        l.unbind_s()
